660304c258834b9b0166926f5191edd1766c4871
baimbrams <baimz.bramz@gmail.com>
2024-03-01 22:52:13 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Card from "./Card";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  Card,
+  Business,
+  Client,
+  CTA,
+  Stats,
+  Footer,
+  Testimonial,
+  Hero,
+};
